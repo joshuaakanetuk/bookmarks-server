64216305ad211d34213afff6c1463f7ef303a4aa
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-08-11 04:56:05 -0400

    Init

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const auth = require('./auth')
+const err = require('./errorHandle')
+const bookmarkRouter = require("./routes/bookmarks");
+
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+    ? 'tiny'
+    : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use(auth)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, boilerplate!')
+})
+
+app.use(err)
+app.use(bookmarkRouter)
+
+module.exports = app
